5583043f88ae82ea5f70fa670407d259903f5dad
Erfan Khodaee <erfankhodaee2013@gmail.com>
2024-08-14 20:01:52 +0330

    solved 01 to 04 examples (TDD)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str,times) {
+  if(times<0){
+    return "ERROR"
+  }
+  let text = ""
+  for(let i = 1; i <= times; i++){
+    text +=str
+  }
+  return text
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+const newArray=[]
+
+array.forEach(rmv)
+
+function rmv(item){
+  if(!args.includes(item)){
+    newArray.push(item)
+  }
+}
+
+return newArray
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
