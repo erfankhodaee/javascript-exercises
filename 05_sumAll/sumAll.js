d0978e91b86a6ed41278bc3ab128b59b5d75eb6f
Erfan Khodaee <erfankhodaee2013@gmail.com>
2024-08-15 01:34:19 +0330

    sum all numbers between 2 numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+  let sum=0;
+  if(a <= 0 || b <= 0 || !(typeof(a)=== "number") || !(typeof(b) === "number") || !((a%1) == 0) || !((b%1) == 0)){
+    return "ERROR"
+  }
+  if (b>a){
+    for(let i = a; i <=b; i++){
+      sum += i
+    }
+  }else if(b<a){
+    for(let i = b; i <= a; i++){
+      sum += i
+    }
+  }
+  return sum
 };
 
 // Do not edit below this line
