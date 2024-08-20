b3bcae0f275c21c6ccb9100c2b01c08260d9b027
Erfan Khodaee <erfankhodaee2013@gmail.com>
2024-08-20 05:11:38 +0330

    added kossheraye nima

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,15 +1,8 @@
 const leapYears = function(year) {
-  if(year % 4 == 0){
-    if(year % 100 == 0){
-      if(year % 400 == 0){
-        return true
-      }
-      return false
-    }
-    return true
-  }
-  return false
-};
-
+ if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))){
+  return true
+ }
+ return false
+}
 // Do not edit below this line
 module.exports = leapYears;
