d7661f16bee85d98529fc403c678ad26df3452e6
Erfan Khodaee <erfankhodaee2013@gmail.com>
2024-09-24 18:32:18 +0330

    solved it

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  word = word.replace(/[^A-Za-z0-9]/g, '').toLowerCase()
+  reverseWord = word.split("").reverse().join("")
+  if(reverseWord == word){
+    return true
+  }else{
+    return false
+  }
 };
 
 // Do not edit below this line
