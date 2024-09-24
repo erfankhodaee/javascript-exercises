0c0c51160eae4e80647270032681d9f7961c9495
Erfan Khodaee <erfankhodaee2013@gmail.com>
2024-09-24 18:34:33 +0330

    solved it with an array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (member) {
+  member = Number(member)
+  if (member < 0) {
+    return "OOPS"
+  } else if (member == 0) {
+    return 0
+  }
+  let myarr = [1, 1]
+  for (let i = 2; i <= (member-1); i++) {
+    myarr[i] = myarr[i-1] + myarr[i-2]
+  }
+  return myarr[member-1]
 
 };
 
