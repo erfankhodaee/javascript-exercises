19dfeb7ea3a9bfd310eba54929084fcd3f937cce
Erfan Khodaee <erfankhodaee2013@gmail.com>
2024-09-24 19:03:03 +0330

    solved without second function and only map

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let age = people.map((person)=>{
+    if(!person.yearOfDeath){
+      person.yearOfDeath = new Date().getFullYear()
+    }
+    return person.yearOfDeath - person.yearOfBirth
+  })
+  let oldest = age.indexOf(Math.max(...age))
+  return people[oldest]
 };
 
 // Do not edit below this line
